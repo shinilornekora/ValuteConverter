a7197d9087c14b4428d623615ed3f12a4eb0373a
Your Name <you@example.com>
2024-04-29 19:13:52 +0300

    renaming things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Calculation } from "./components/Calculation/calculation";
-import { CalculationHistory } from "./components/CalculationHistory/calculationHistory";
+import { Calculation } from "./components/Calculation/Сalculation";
+import { CalculationHistory } from "./components/CalculationHistory/СalculationHistory";
 
 import './styles/app.css'
 
