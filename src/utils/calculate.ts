2acbd8a9136c2ef3899e52118f4adaaa9e77cc9f
Your Name <you@example.com>
2024-04-29 23:05:13 +0300

    add rxjs

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -1,43 +1,52 @@
-import { CalculateProps } from "@types";
+import { CalculateProps } from '@types';
+import { from as _from, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
-const apiUrl = 'https://www.cbr-xml-daily.ru/daily_json.js'
+const apiUrl = 'https://www.cbr-xml-daily.ru/daily_json.js';
 
-export async function calculate({ from, to, setHistory, setResult }: CalculateProps) {
-    const secondVal = await _calculateHelper(from, to);
+export function calculate({ from, to, setHistory, setResult }: CalculateProps) {
+    _from(_calculateHelper(from, to)).pipe(
+        switchMap((secondVal: number) => {
+            if (secondVal !== -1) {
+                const date = new Date();
 
-    const date = new Date();
+                const options: Intl.DateTimeFormatOptions = {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                };
 
-    const options: Intl.DateTimeFormatOptions = {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-    };
+                const dateString = date.toLocaleDateString('ru-RU', options);
 
-    const dateString = date.toLocaleDateString('ru-RU', options);
-
-    if (secondVal !== -1) {
-        setHistory({
-            type: "ADD_TO_HISTORY", payload: {
-                from: from[1],
-                to: to,
-                first_val: from[0]!,
-                second_val: secondVal,
-                date: dateString,
+                setHistory({
+                    type: "ADD_TO_HISTORY",
+                    payload: {
+                        from: from[1],
+                        to: to,
+                        first_val: from[0]!,
+                        second_val: secondVal,
+                        date: dateString,
+                    }
+                });
             }
-        })
-    }
 
-    setResult(secondVal);
+            setResult(secondVal);
+
+            return of(null);
+        }),
+        catchError((error: Error) => {
+            console.error('An error occurred:', error);
+            setResult(-1);
+
+            return of(null);
+        })
+    ).subscribe();
 }
 
-/**
- * Функция-хелпер, инкапсулирует логику подсчета результата.
- * Вроде бы учел все corner-кейсы, но может что-то и просочится.
- */
-async function _calculateHelper(from: [number | undefined, string ], to: string) {
+async function _calculateHelper(from: [number | undefined, string], to: string): Promise<number> {
     if (from[0] === undefined || from[0] < 0) {
         return -1;
     }
@@ -50,9 +59,9 @@ async function _calculateHelper(from: [number | undefined, string ], to: string)
         return from[0];
     }
 
-    const data = await fetch(apiUrl).then(value => value.json());
+    const data = await fetch(apiUrl).then(response => response.json());
 
-    if (from[1] === "RUB" ) {
+    if (from[1] === "RUB") {
         return from[0] * (1 / (data?.Valute[to]["Value"] / data?.Valute[to]["Nominal"]));
     }
 
@@ -60,8 +69,8 @@ async function _calculateHelper(from: [number | undefined, string ], to: string)
         return from[0] * (data?.Valute[from[1]]["Value"] / data?.Valute[from[1]]["Nominal"]);
     }
 
-    const first = 1 / (data?.Valute[from[1]]["Value"] / data?.Valute[from[1]]["Nominal"])
-    const second = 1 / (data?.Valute[to]["Value"] / data?.Valute[to]["Nominal"])
+    const first = 1 / (data?.Valute[from[1]]["Value"] / data?.Valute[from[1]]["Nominal"]);
+    const second = 1 / (data?.Valute[to]["Value"] / data?.Valute[to]["Nominal"]);
 
-    return Math.round((second / first) * from[0]*1000) / 1000;
+    return Math.round((second / first) * from[0] * 1000) / 1000;
 }
